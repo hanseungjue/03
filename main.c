828ba92ddd891d501860972c68617edccb137891
hanseungjue <hansj0628@naver.com>
2024-09-11 14:23:20 +0900

    week02

--- main.c
+++ main.c
@@ -4,14 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b;
+	int a,b;
+    a=10;
+    b=20;
+    printf("%d + %d=%d\n",a,b,a+b);
+	return 0;
+	
+	return 0;
+}
+
+/*int a, b;
 	printf("분자를 입력하세요 :");
 	scanf("%i",  &a);
 	
 	printf("분모를 입력하세요 :");
 	scanf("%i", &b);
 	
-	printf("나누기의 결과는 %f 입니다", (float)a/b);
-	
-	return 0;
-}
+	printf("나누기의 결과는 %f 입니다", (float)a/b);*/
